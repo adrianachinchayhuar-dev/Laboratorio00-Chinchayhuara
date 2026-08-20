99cb69d1d7819747d5391a4449a97318c8cb1be8
Adriana Chinchayhuara <adriana.chinchayhuar@tecsup.edu.pe>
2026-08-20 12:32:34 -0500

    Initial Commit

---
+++ Laboratorio00-Chinchayhuara/Laboratorio00_ChinchayhuaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Laboratorio00_ChinchayhuaraApp.swift
+//  Laboratorio00-Chinchayhuara
+//
+//  Created by Tecsup on 20/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Laboratorio00_ChinchayhuaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
